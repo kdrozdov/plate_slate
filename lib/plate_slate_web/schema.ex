d440aee670632e493279d2979415432228ffd2da
Kirill Drozdov <kirill.dzv@gmail.com>
2020-05-28 05:50:28 +0500

    Added graphql schema

---
+++ lib/plate_slate_web/schema.ex
@@ -0,0 +1,21 @@
+defmodule PlateSlateWeb.Schema do
+  use Absinthe.Schema
+
+  alias PlateSlate.Menu
+
+  query do
+    @desc "The list of available items on the menu"
+    field :menu_items, list_of(:menu_item) do
+      resolve fn _, _, _ ->
+        {:ok, Menu.list_items()}
+      end
+    end
+  end
+
+  object :menu_item do
+    field :id, :id
+    field :name, :string
+    field :description, :string
+  end
+end
+
